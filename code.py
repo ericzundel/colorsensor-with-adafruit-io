423bdaa70349f7df234919725c3dec06cad2e44e
Eric Z. Ayers <ericzundel@gmail.com>
2024-01-11 11:48:35 -0500

    Added in temperature sensor and user prompt

--- code.py
+++ code.py
@@ -2,12 +2,15 @@
 #
 # SPDX-License-Identifier: MIT
 
-
-import os
+import board
+import busio
 import ipaddress
-import wifi
+import os
 import socketpool
 import time
+import wifi
+
+import adafruit_tcs34725
 
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_io.adafruit_io import IO_MQTT
@@ -18,6 +21,18 @@ try:
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there and don't commit them to git!")
     raise
+
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = busio.I2C(board.GP1, board.GP0)  # uses board's native I2C port
+sensor = adafruit_tcs34725.TCS34725(i2c)
+
+
+# Change sensor integration time to values between 2.4 and 614.4 milliseconds
+sensor.integration_time = 150
+
+# Change sensor gain to 1, 4, 16, or 60
+sensor.gain = 4
 
 print()
 print("Connecting to WiFi")
@@ -68,6 +83,22 @@ def message(client, topic, message):
     """
     print(f"New message on topic {topic}: {message}")
 
+def read_color_input():
+    """Ask the human to select the color"""
+    while True:
+        print("What color?  (R)ed (P)urple (O)range (Y)ellow (G)reen [R/P/O/Y/G]: ")
+        response = input().upper()
+        if response is 'R':
+            return 'red'
+        elif response is 'P':
+            return 'purple'
+        elif response is 'O':
+            return 'orange'
+        elif response is 'Y':
+            return 'yellow'
+        elif response is 'G':
+            return 'green'
+
 # Get a socket from the socket pool
 socket_pool = socketpool.SocketPool(wifi.radio)
 
@@ -105,10 +136,32 @@ while True:
         io.reconnect()
         continue
 
+    train_color = read_color_input()
+
+    print(
+        "RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(
+             sensor.color, sensor.color_rgb_bytes
+        )
+    )
+
     # Send a new message
     print("Sending value to feed '%s'" % (default_topic))
-    io.publish(default_topic, "{'temperature': 3000, 'r' : 100, 'g': 10, 'b': 1, 'lux' : 100, 'color': 'orange'}")
-    #time.sleep(10)]
+
+    # This is dummy data, but in the format I anticipate we will use for the training machine.
+    io.publish(default_topic, "{'temperature': %d, 'r' : %d, 'g': %d, 'b': %d, 'lux' : %d, 'color': '%s'}" %
+        (
+            sensor.color_temperature,
+            sensor.color_rgb_bytes[0],
+            sensor.color_rgb_bytes[1],
+            sensor.color_rgb_bytes[2],
+            sensor.lux,
+            train_color
+        )
+    )
+
+    # To do this in a loop, we have to make sure not to exceed the adafruit io limits. For the free plan that's
+    # 30 samples per minute.
+    #time.sleep(2)]
     break
 
 print("View data at https://io.adafruit.com/%s/feeds/colorsensor-training-data" % (secrets["aio_username"]))
